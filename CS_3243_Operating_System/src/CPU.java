e367d94c33a029882cf0aadb8afad921b4cb6034
havoc.2005@gmail.com <havoc.2005@gmail.com@d45fb3eb-81c0-fc5d-01a1-4b05ad1b335a>
2014-02-03 23:29:15 +0000

    Added CPU file. Going to add more methods to it.

---
+++ CS_3243_Operating_System/src/CPU.java
@@ -0,0 +1,25 @@
+
+public class CPU {
+	static int pc; // this variable is a program counter 
+	static int[][] register;
+	static int baseRegister;
+	static int limitRegister;
+	public CPU(){
+		pc=0;
+		register=new int[16][32];
+		
+
+	}
+	private void begin(){
+		
+	}
+	private void fetch(int lineRam){
+		
+	}
+	private void decode(){
+		
+	}
+	private void execute(){
+		
+	}
+}
