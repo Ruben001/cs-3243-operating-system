14bfe9932e7b8bd26a81165672d7d7e0ee86c464
ivusilvan@gmail.com <ivusilvan@gmail.com@d45fb3eb-81c0-fc5d-01a1-4b05ad1b335a>
2014-03-02 16:06:32 +0000

    -Updated by: Ivan Mba
    -Added UI functionality.
    
    -To run application, run the class GUI.java, then input the filename in case you are not comfortable with the default, then click on run
    
    -All output stored in a file, so when working on CPU might notice some changes(File does not necessarily need to exist as it will create in case not, so no worries)
    
    -In case, input file does not exist, an error is displayed
    
    -Alternatively, to run the application form the OSDriver and have the results displayed in the console, uncomment the line in the OSDriver which reads "uncomment this line...." then run the OSDriver

--- trunk/CS_3243_Operating_System/src/OSDriver.java
+++ trunk/CS_3243_Operating_System/src/OSDriver.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 
 
 public class OSDriver {
@@ -8,12 +16,22 @@ public class OSDriver {
 	private static Memory memory;
 	private static LongTermScheduler ltScheduler;
 	private static ShortTermScheduler stScheduler;
-
+	public static String content;
+	private static String filename;
 	// list of all PCB's remaining in the disk
 	private static ArrayList<PCB> pcbList;
 	
 	// queues
 	private static ArrayList<PCB> readyQueue;
+	
+	
+	public static void setFileName(String name){
+		
+		filename = name;
+		
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		// initialize components
@@ -22,19 +40,67 @@ public class OSDriver {
 		cpu = new CPU(memory,stScheduler);
 		pcbList = new ArrayList<PCB>();
 		readyQueue = new ArrayList<PCB>();
+		
+		//filename = "DataFile2-Cleaned.txt";//if you don't want to use the GUI, uncomment this line
 
 		ltScheduler = new LongTermScheduler(disk, memory, pcbList, readyQueue, SchedulingAlgorithm.FCFS);
 		stScheduler = new ShortTermScheduler(cpu,ltScheduler,memory,pcbList,readyQueue,SchedulingAlgorithm.FCFS);
 		// call the loader to load the job file into the Disk
-		Loader loader = new Loader("DataFile2-Cleaned.txt", disk, pcbList);
+		
+		try{
+		Loader loader = new Loader(filename, disk, pcbList);
 		loader.load();
+		
+		}
+		
+		catch(Exception e){
+			
+		
+			JOptionPane.showMessageDialog(null, "Sorry, the file was not found or a wrong path was entered", "File not found", JOptionPane.ERROR_MESSAGE);
+			
+		}
+		
 		
 		
 		
 		ltScheduler.schedule();
 		stScheduler.ScheduleAndDispatch();
 		
+		content = "";
 		
+		try{
+			
+			File f = new File("results.txt");
+			FileReader fr = new FileReader("results.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String lineRead = br.readLine();
+			
+			content = lineRead;
+			
+			while(lineRead!=null){
+				
+				lineRead = br.readLine();
+				
+				if(lineRead!=null){
+				content = content + "\n" + lineRead;
+				System.out.println(lineRead);
+				}
+			}
+			
+			br.close();
+			f.delete();
+			
+		}
+		catch(FileNotFoundException fnfe){
+			
+			System.out.println("File not found");
+			
+		}
+		catch(IOException ioe){
+			
+			ioe.printStackTrace();
+		}
 		return;
 	}
 
