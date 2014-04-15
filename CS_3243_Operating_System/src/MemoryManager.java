35e7097cf6168cb81d6776db8a11c24be5ccc529
havoc.2005@gmail.com <havoc.2005@gmail.com@d45fb3eb-81c0-fc5d-01a1-4b05ad1b335a>
2014-04-15 10:34:26 +0000

    Updated MemoryManager.java with freePageList and freeFrameList methods

--- CS_3243_Operating_System/src/MemoryManager.java
+++ CS_3243_Operating_System/src/MemoryManager.java
@@ -3,14 +3,16 @@ public class MemoryManager {
 	
 	private static Disk disk;
 	private static Memory ram;
-	private static int totalPageNumber, totalFrameNumber, pageNumber, frameNumber;
+	private static int totalPageNumber, totalFrameNumber;
 	private ArrayList<JobFrame> frames;
 	private final int PAGE_SIZE = 16;
 	private static PageTable[] pageTable;
+	private ArrayList<Integer> freePageList, freeFrameList;
+	
 	
 	public MemoryManager(){
 		frames = new ArrayList<JobFrame>();
-		
+		freeFrameList = new ArrayList<Integer>();
        
         disk = new Disk();
         ram = new Memory();
@@ -19,16 +21,38 @@ public class MemoryManager {
         totalFrameNumber = 2048/PAGE_SIZE;
         
         pageTable = new PageTable[totalPageNumber];
+        freePageList = new ArrayList<Integer>();
+        
         initializePageTable();
+        initializeFreePageList();
+        initializeFreeFrameList();
         
 	}
 	
+	/**
+	 * This method initialize freeFrameList
+	 */
+	private void initializeFreeFrameList() {
+		for(int i =0; i<totalFrameNumber;i++){
+			freeFrameList.add(i);
+		}
+		
+	}
+
 	/*
 	 * This method initialize page table for the first time
 	 */
 	private void initializePageTable(){
 		for(int i =0;i<totalPageNumber;i++){
 			pageTable[i]= new PageTable();
+		}
+	}
+	/**
+	 * This method initialize freePageList
+	 */
+	private void initializeFreePageList(){
+		for(int i=0;i<totalPageNumber;i++){
+			freePageList.add(i);
 		}
 	}
 	
@@ -49,6 +73,43 @@ public class MemoryManager {
 	}
 	public boolean isValidFrame(int pageNumber){
 		return pageTable[pageNumber].isValid();
+	}
+	
+	/**
+	 * This methods gives the first free page number
+	 * returns -1 if pages are not free
+	 */
+	public int getFreePageNumber(){
+		if(freePageList.size()>0){
+			return freePageList.get(0);
+		}
+		return -1;
+	}
+	
+	/**
+	 * This methods gives the first free frame number
+	 * returns -1 if frames are not free
+	 */
+	public int getFreeFrameNumber(){
+		if(freeFrameList.size()>0){
+			return freeFrameList.get(0);
+		}
+		return -1;
+	}
+	
+	/**
+	 * This methods free page from page table, and also free memory of ram
+	 * @param pageNumber that would be free
+	 */
+	public void freePage(int pageNumber){
+		int address = pageNumber*PAGE_SIZE;
+		int length = PAGE_SIZE;
+		pageTable[pageNumber].setValid(false);
+		pageTable[pageNumber].setFrameNumber(0);
+		pageTable[pageNumber].setJobID(0);
+		
+		
+		ram.free(address, length);
 	}
 	/****************************************************************************************
 	* Reading and writing data to the disk and ram methods									*
